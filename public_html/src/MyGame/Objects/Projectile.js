a18f3e3a1fb580d9fa2d3f541ddb657542726646
Chris Johnston <Chris@thejohnstons.net>
2018-03-05 13:23:18 -0800

    Add projectile class

---
+++ public_html/src/MyGame/Objects/Projectile.js
@@ -0,0 +1,51 @@
+/**
+ * Projectile class
+ *
+ * A generic class that can collide with another class
+ *
+ * Adds the interface for onHit and isDead
+ */
+
+"use strict";
+
+/**
+ * Constructor
+ * Sets the initial value of the is dead and is hit values to be both false
+ *
+ * @param renderable {Renderable} The renderable object to pass on to the game object
+ * @constructor
+ */
+function Projectile(renderable)
+{
+    this.mIsDead = false;
+    this.mIsHit = false;
+};
+
+gEngine.Core.inheritPrototype(Projectile, GameObject);
+
+/**
+ * Is the object dead? If so, then this object should be removed from the set of objects.
+ * @returns {boolean}
+ */
+Projectile.prototype.isDead = function()
+{
+    return this.mIsDead;
+};
+
+/**
+ * Has the object been hit? If so, do something
+ * @returns {boolean}
+ */
+Projectile.prototype.isHit = function()
+{
+    return this.mIsHit;
+};
+
+/**
+ * This function should be called when the projectile hits something
+ */
+Projectile.prototype.onHit = function()
+{
+    // default to doing nothing, let each object that subclasses this type deal with the implementation
+    this.mIsHit = true;
+};
