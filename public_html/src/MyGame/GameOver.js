bd2bdbce0315908bdc62166d582a6db1b1f7e947
Chris Johnston <chris@thejohnstons.net>
2018-03-05 16:31:26 -0800

    fix the camera size for the game over and the win screen

--- public_html/src/MyGame/GameOver.js
+++ public_html/src/MyGame/GameOver.js
@@ -29,7 +29,7 @@ GameOver.prototype.initialize = function () {
     this.mCamera = new Camera(
         vec2.fromValues(50, 40), // position of the camera
         100,                     // width of camera
-        [0, 0, 800, 600]         // viewport (orgX, orgY, width, height)
+        [0, 0, 1000, 700]         // viewport (orgX, orgY, width, height)
     );
     this.mCamera.setBackgroundColor([0, 0, 1, 1]);
     
